4e91356dfbf2c397e7f4ab0a623038216a351679
Lillie Dae <lillie.dae@answerdigital.com>
2022-06-01 10:47:12 +0100

    further merge fixs
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

---
+++ src/Storage/Common/Policies/Policy.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.Storage.Common.Policies
+{
+    public partial class Policy
+    {
+        public string Version { get; set; } = "2012-10-17";
+
+        public IList<Statement> Statement { get; set; } = new List<Statement>();
+
+        public static Policy? FromJson(string json) => JsonConvert.DeserializeObject<Policy>(json, Converter.Settings);
+    }
+}
---
+++ src/Storage/Common/Policies/StringEquals.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using Newtonsoft.Json;
+
+namespace Monai.Deploy.Storage.Common.Policies
+{
+    public class StringEquals
+    {
+        [JsonProperty(PropertyName = "s3:prefix")]
+        public string[]? S3Prefix { get; set; }
+
+        [JsonProperty(PropertyName = "s3:delimiter")]
+        public string[]? S3Delimiter { get; set; }
+    }
+}
