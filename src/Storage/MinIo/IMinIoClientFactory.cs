a19abb6f47eeef8b04fd44c64ecfde9d3a14864e
Jack Schofield <jack.schofield@answerdigital.com>
2022-06-06 15:48:36 +0100

    add factory pattern for minio
    Signed-off-by: Jack Schofield <jack.schofield@answerdigital.com>

---
+++ src/Storage/MinIo/IMinIoClientFactory.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Minio;
+
+namespace Monai.Deploy.Storage.MinIo
+{
+    public interface IMinIoClientFactory
+    {
+        MinioClient GetClient();
+    }
+}
