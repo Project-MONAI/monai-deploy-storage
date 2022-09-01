5b37ce4567228312a4d6ed4278e60edc07dcffdf
Lillie Dae <61380713+lillie-dae@users.noreply.github.com>
2022-09-01 16:23:33 +0100

    Minio admin healthcheck fix (#61)
    * minor fixes to admin health checks
    
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/Plugins/MinIO/LoggerMethods.cs
+++ src/Plugins/MinIO/LoggerMethods.cs
@@ -20,8 +20,8 @@ namespace Monai.Deploy.Storage.MinIO
 {
     public static partial class LoggerMethods
     {
-        [LoggerMessage(EventId = 20000, Level = LogLevel.Error, Message = "Error listing objects in bucket '{bucketName}'.")]
-        public static partial void ListObjectError(this ILogger logger, string bucketName);
+        [LoggerMessage(EventId = 20000, Level = LogLevel.Error, Message = "Error listing objects in bucket '{bucketName}' with error: {error}")]
+        public static partial void ListObjectError(this ILogger logger, string bucketName, string error);
 
         [LoggerMessage(EventId = 20001, Level = LogLevel.Error, Message = "File '{path}' could not be found in '{bucketName}'.")]
         public static partial void FileNotFoundError(this ILogger logger, string bucketName, string path);
